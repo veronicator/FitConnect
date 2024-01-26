3bc3494cef66cb86992469f682adfd79cfcdb0e9
Tommaso Bertini <tommyb.bertini@gmail.com>
2024-01-26 18:13:19 +0100

    fix login controller

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/controller/LoginController.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/controller/LoginController.java
@@ -1,41 +1,19 @@
 package it.unipi.dsmt.FitConnect.controller;
 
-import it.unipi.dsmt.FitConnect.entities.User;
+
+import it.unipi.dsmt.FitConnect.entities.LdapUser;
+import it.unipi.dsmt.FitConnect.entities.MongoUser;
 import it.unipi.dsmt.FitConnect.services.AuthService;
-import it.unipi.dsmt.FitConnect.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class LoginController {
      @Autowired
-     UserService userService;
-//     @Autowired
-//     AuthService authService;
-
-//     @GetMapping("/debug")
-//     public String debug(Model model) throws NoSuchAlgorithmException {
-//         List<User> users = userService.findAllUsers();
-//         List<String> hashedpsws = new ArrayList<>();
-//         for(User user : users){
-//             String s = user.getPassword();
-//             hashedpsws.add(authService.hashPassword(s));
-//         }
-//
-//         model.addAttribute("users", users);
-//         model.addAttribute("passwords", hashedpsws);
-//         return "debug";
-//     }
+     AuthService authService;
 
     @GetMapping("/home")
     public String login() {
@@ -51,16 +29,12 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup(){ return "signup"; }
 
-
-    /*
-    * Login handler
-    */
     @PostMapping("/login")
     public String doLogin(@RequestParam String username,
                           @RequestParam String password,
                           Model model) {
 
-        User loggedUser = userService.authenticate(username, password);
+        MongoUser loggedUser = authService.authenticate(username, password);
 
         if(loggedUser == null)
             return "login";
@@ -70,34 +44,28 @@ public class LoginController {
         }
     }
 
-    /*
-     * Signup handler
-     */
     @PostMapping("/signup")
-    public String doSignup(@RequestParam String name, @RequestParam String surname,
+    public String doSignup(@RequestParam String firstName, @RequestParam String lastName,
                            @RequestParam String email, @RequestParam String username,
-                           @RequestParam String password, @RequestParam String repeat_password,
-                           Model model) {
-
+                           @RequestParam String password, @RequestParam String repeat_password)
+    {
         if(!password.equals(repeat_password)) {
             System.out.println("Password mismatch. Retry");
             return "signup";
         }
 
-//        if(userService.search(username) != null){
-//            System.out.println("Username already used. Retry");
-//            return "signup";
-//        }
-
         try{
-            User newUser = new User(username, name, surname, password, email);
-
-            userService.signup(newUser);
+            String commonName = String.join(" ", firstName, lastName);
+            LdapUser ldapUser = new LdapUser(username, commonName, lastName, password);
+            MongoUser mongoUser = new MongoUser(username, firstName, lastName, email);
+            //todo: gestire ruolo
+            mongoUser.setRole("client");
+            authService.signup(ldapUser, mongoUser);
 
             return "login";
 
         } catch (Exception e){
-            System.out.println("Signup error: " + e.getMessage());
+            System.out.println("Error in signup: " + e.getMessage());
             return "signup";
         }
     }
