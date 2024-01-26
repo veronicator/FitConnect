638b7d5637706bb61538f347cfa6cecc52711c1d
Tommaso Bertini <tommyb.bertini@gmail.com>
2024-01-26 18:12:33 +0100

    fix services

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/services/UserService.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/services/UserService.java
@@ -1,142 +1,94 @@
 package it.unipi.dsmt.FitConnect.services;
 
-import it.unipi.dsmt.FitConnect.entities.User;
-import it.unipi.dsmt.FitConnect.repositories.LdapUserRepository;
-import it.unipi.dsmt.FitConnect.repositories.MongoUserRepository;
-import it.unipi.dsmt.FitConnect.util.SecurityManager;
+
+import it.unipi.dsmt.FitConnect.entities.MongoUser;
+import it.unipi.dsmt.FitConnect.repositories.mongo.MongoUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.DirContextAdapter;
-import org.springframework.ldap.core.LdapTemplate;
-import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.stereotype.Service;
 
-import javax.naming.Name;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    private Environment env;
-
-    @Autowired
-    private ContextSource contextSource;
-
-    @Autowired
-    private LdapTemplate ldapTemplate;
-
-    @Autowired
-    private LdapUserRepository ldapUserRepository;
-
-    @Autowired
     private MongoUserRepository mongoUserRepository;
 
-    @Autowired
-    private SecurityManager securityManager;
+    public Optional<MongoUser> findById(String id) {
+        return mongoUserRepository.findById(id);
+    }
 
-    public User authenticate(final String username, final String password) {
-        User user = ldapUserRepository.findByUsernameAndPassword(username, securityManager.digestSHA(password));
-        if(user == null) {
-            System.out.println("Authentication failed");
-            System.out.println("Failed in ldap database");
-            return null;
+    public Optional<MongoUser> findByEmail(String email){
+        return mongoUserRepository.findByEmail(email);
+    }
+
+    public Optional<MongoUser> findByUsername(String username) {
+        return mongoUserRepository.findByUsername(username);
+    }
+    public List<MongoUser> findAllUsers() {
+        return mongoUserRepository.findAll();
+    }
+
+    public List<MongoUser> findAllByFirstName(String firstname) {
+        return mongoUserRepository.findByFirstName(firstname);
+    }
+
+    public List<MongoUser> findAllByLastName(String lastname) {
+        return mongoUserRepository.findByLastName(lastname);
+    }
+
+    public List<MongoUser> findByRole(String role) {
+        return mongoUserRepository.findByRole(role);
+    }
+
+    public boolean existsByFirstName(String firstname) {
+        return mongoUserRepository.existsByFirstName(firstname);
+    }
+
+    public boolean existsByLastName(String lastname) {
+        return mongoUserRepository.existsByLastName(lastname);
+    }
+
+    public boolean existsByEmail(String email) {
+        return mongoUserRepository.existsByEmail(email);
+    }
+
+    public boolean existsByUsername(String username) {
+        return mongoUserRepository.existsByUsername(username);
+    }
+
+    public void deleteById(String id) {
+        mongoUserRepository.deleteById(id);
+    }
+    public void deleteByEmail(String email) {
+        mongoUserRepository.deleteByEmail(email);
+    }
+
+    public void deleteUser(MongoUser user) {
+        mongoUserRepository.delete(user);
+    }
+
+    public boolean updateEmail(String oldEmail, String newEmail) {
+        if(existsByEmail(newEmail)) {
+            System.out.println("Email address already used by another account");
+            return false;
         }
-        else {
-            user = mongoUserRepository.findByUsername(username);
-            if(user == null){
-                System.out.println("Authentication failed");
-                System.out.println("Failed in mongo database");
-                return null;
-            }
-            else{
-                System.out.println("Authenticaiton succeded!");
-                return user;
-            }
-        }
+        mongoUserRepository.updateEmail(oldEmail, newEmail);
+        return true;
     }
 
-    public void create(final User user) {
-        System.out.println("create...");
 
-        Name dn = LdapNameBuilder
-                .newInstance()
-                .add("dc", "com").add("dc", "baeldung").add("ou", "people").add("uid", user.getUsername())
-                .build();
 
-        DirContextAdapter context = new DirContextAdapter(dn);
-        context.setAttributeValues("objectclass", new String[]{"top", "person", "organizationalPerson", "inetOrgPerson"});
-        context.setAttributeValue("cn", user.getFirstName());
-        context.setAttributeValue("sn", user.getLastName());
-        context.setAttributeValue("userPassword", securityManager.digestSHA(user.getPassword()));
 
-        System.out.println(context);
 
-//        try {
-//            // insert user into ldap db
-//            ldapTemplate.bind(context);
-//            // insert user into mongo db
-//            mongoUserRepository.save(user);
-//            System.out.println("User: " + user.getUsername() + " registered");
-//        } catch (Exception e) {
-//            System.err.println("Error creating user: " + e.getMessage());
-//        }
 
-        // insert user into ldap db
-        ldapTemplate.bind(context);
-        // insert user into mongo db
-        mongoUserRepository.save(user);
-        System.out.println("User: " + user.getUsername() + " registered");
-    }
 
-    public User search(final String username) {
 
-        User user = ldapUserRepository.findByUsername(username);
-        System.out.println("Found user: " + user);
 
-        return user;
-    }
 
-    public void modify(final String username, final String password) {
-        try{
-            User user = ldapUserRepository.findByUsername(username);
-            user.setPassword(securityManager.digestSHA(password));
-            ldapUserRepository.save(user);
-            System.out.println("Password modified");
-        } catch (Exception e) {
-            System.err.println("Error modifying user: " + e.getMessage());
-        }
 
-    }
 
-    public void signup(final User newUser) throws Exception {
-
-        // check if username is already used
-        if(search(newUser.getUsername()) != null){
-            throw new Exception("Username already used. Retry");
-        }
-
-//        try{
-//            //add new user in ldap db
-//            create(newUser);
-//
-//            //add new user in mongodb
-//            mongoUserRepository.save(newUser);
-//        }
-//        catch (Exception e){
-//            throw new Exception(e.getMessage());
-//        }
-
-        //add new user in ldap db
-        create(newUser);
-
-        //add new user in mongodb
-        mongoUserRepository.save(newUser);
-
-    }
 
 
 
--- fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/util/SecurityManager.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/util/SecurityManager.java
@@ -5,16 +5,22 @@ import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.Base64;
+
 @Service
 @NoArgsConstructor
 public class SecurityManager {
 
-    public String digestSHA(final String password) {
+    /**
+     * Hashes a password using the SHA algorithm and encodes it in Base64 with the "{SHA}" prefix.
+     *
+     * @param password the password to hash
+     * @return the hashed password with the "{SHA}" prefix
+     */
+    public String hashPassword(final String password){
         String base64;
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA256");
+            MessageDigest digest = MessageDigest.getInstance("SHA");
             digest.update(password.getBytes());
             base64 = Base64
                     .getEncoder()
@@ -22,39 +28,10 @@ public class SecurityManager {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+
         return "{SHA}" + base64;
     }
 
-//    public String get_SHA_256_SecurePassword(String passwordToHash,
-//                                                     String salt) {
-//        String generatedPassword = null;
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("SHA-256");
-//            md.update(salt.getBytes());
-//            byte[] bytes = md.digest(passwordToHash.getBytes());
-//            StringBuilder sb = new StringBuilder();
-//            for (int i = 0; i < bytes.length; i++) {
-//                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
-//                        .substring(1));
-//            }
-//            generatedPassword = sb.toString();
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        return generatedPassword;
-//    }
-//
-//    // Add salt
-//    public String getSalt() throws NoSuchAlgorithmException {
-//        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-//        byte[] salt = new byte[16];
-//        sr.nextBytes(salt);
-//        return salt.toString();
-//    }
-//
-//    public String getStaticSalt(){
-//        return "my_static_salt";
-//    }
 
 }
 
