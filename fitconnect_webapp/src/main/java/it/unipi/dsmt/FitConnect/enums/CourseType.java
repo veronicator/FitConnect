e5f7c567127b93efffac3619dd8fbbac8df178e7
veronicator <80213270+veronicator@users.noreply.github.com>
2024-02-03 22:01:19 +0100

    added CourseType enumerator

---
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/enums/CourseType.java
@@ -0,0 +1,28 @@
+package it.unipi.dsmt.FitConnect.enums;
+
+public enum CourseType {
+    Yoga,
+    Swim,
+    Crossfit,
+    FunctionalTraining,
+    Calisthenics,
+    BodyBuilding,
+    FitnessCourse,
+    Gymnastic,
+    HighIntensityIntervalTraining,
+    MartialArts,
+    Judo,
+    KickBoxing,
+    MMA,
+    RhythmicGymnastics,
+    ArtisticGymnastics,
+    PoleDance,
+    Ballet,
+    HipHopDance,
+    ModernDance,
+    LatinDance,
+    SwingDance,
+    StreetDance,
+    Other
+
+}
