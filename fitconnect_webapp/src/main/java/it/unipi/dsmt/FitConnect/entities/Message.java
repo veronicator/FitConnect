9833542f3d9ce5ec41253e3e0f8af4481e08e001
veronicator <80213270+veronicator@users.noreply.github.com>
2024-02-03 22:11:59 +0100

    edit Message class

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/entities/Message.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/FitConnect/entities/Message.java
@@ -2,22 +2,37 @@ package it.unipi.dsmt.FitConnect.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "messages")
 public class Message {
+    @Id
+    private String id;
+    @DocumentReference(collection = "courses")
+    private Course course;
     @DocumentReference(collection = "users")
     private MongoUser sender;
-    private Instant sendTime;
+    private LocalDateTime sendTime;
     private String text;
 
+    public Message (MongoUser sender, LocalDateTime sendTime, String text) {
+        this.sender = sender;
+        this.sendTime = sendTime;
+        this.text = text;
+    }
     public Message (MongoUser sender, String text) {
-        this(sender, Instant.now(), text);
+        this(sender, LocalDateTime.now(), text);
     }
 
 }
