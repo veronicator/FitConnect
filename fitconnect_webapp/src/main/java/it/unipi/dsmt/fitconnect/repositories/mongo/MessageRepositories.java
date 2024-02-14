7262a7fc6e9147da037d0cb29f4636ef5dd465fb
veronicator <80213270+veronicator@users.noreply.github.com>
2024-02-14 21:42:21 +0100

    msg repo

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/repositories/mongo/MessageRepositories.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/repositories/mongo/MessageRepositories.java
@@ -1,9 +1,16 @@
 package it.unipi.dsmt.fitconnect.repositories.mongo;
 
 import it.unipi.dsmt.fitconnect.entities.Message;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageRepositories extends MongoRepository<Message, String> {
+
+    @Query("{'course': ?0, 'sender': { $regex: ?1, $options: 'i'} }")
+    List<Message> findByCourseAndSender(ObjectId courseId, String sendUsername);
 }
