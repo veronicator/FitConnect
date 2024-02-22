0521fd3736614c19d319714c02b491e8f716459b
veronicator <80213270+veronicator@users.noreply.github.com>
2024-02-22 11:46:38 +0100

    added restService file

---
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/services/RestService.java
@@ -0,0 +1,14 @@
+package it.unipi.dsmt.fitconnect.services;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestService {
+    private final RestTemplate restTemplate;
+
+    public RestService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+}
