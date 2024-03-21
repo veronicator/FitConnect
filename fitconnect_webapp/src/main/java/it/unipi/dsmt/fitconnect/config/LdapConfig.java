7abe6fae755d7a0f5277a8dede083c96ff79e668
veronicator <80213270+veronicator@users.noreply.github.com>
2024-03-21 19:19:16 +0100

    removed hardcoded ldap property from LdapConfig class (values now taken through the @Value annotation from the application.properties) + added try-catch in ErlangNodesController + clean

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/config/LdapConfig.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/config/LdapConfig.java
@@ -1,6 +1,7 @@
 package it.unipi.dsmt.fitconnect.config;
 
 import it.unipi.dsmt.fitconnect.services.AuthService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.ldap.core.LdapTemplate;
@@ -13,13 +14,26 @@ import org.springframework.ldap.core.support.LdapContextSource;
 @EnableLdapRepositories(basePackages = "it.unipi.dsmt.fitconnect.repositories.ldap.*")
 public class LdapConfig {
 
+    @Value("${spring.ldap.urls}")
+    private String url;
+    @Value("${spring.ldap.username}")
+    private String userDn;
+    @Value("${spring.ldap.password}")
+    private String pwd;
+
     @Bean
     public LdapContextSource contextSource() {
 
         LdapContextSource contextSource = new LdapContextSource();
-        contextSource.setUrl("ldap://10.2.1.78:389"); // Adjust the URL based on your LDAP server configuration
-        contextSource.setUserDn("cn=admin,dc=fitconnect,dc=com"); // Adjust the DN based on your LDAP server configuration
-        contextSource.setPassword("password"); // Adjust the password based on your LDAP server configuration
+//        contextSource.setUrl("ldap://10.2.1.78:389"); // Adjust the URL based on your LDAP server configuration
+//        contextSource.setUserDn("cn=admin,dc=fitconnect,dc=com"); // Adjust the DN based on your LDAP server configuration
+//        contextSource.setPassword("password"); // Adjust the password based on your LDAP server configuration
+
+
+        contextSource.setUrl(url); // Adjust the URL based on your LDAP server configuration
+        contextSource.setUserDn(userDn); // Adjust the DN based on your LDAP server configuration
+        contextSource.setPassword(pwd); // Adjust the password based on your LDAP server configuration
+
         return contextSource;
     }
 
