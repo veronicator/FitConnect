e956d3b428ce3ed57bf5bdce1d0978218b21f849
veronicator <80213270+veronicator@users.noreply.github.com>
2024-03-28 19:06:46 +0100

    clean some code

--- fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/config/MongoConfig.java
+++ fitconnect_webapp/src/main/java/it/unipi/dsmt/fitconnect/config/MongoConfig.java
@@ -1,36 +1,18 @@
 package it.unipi.dsmt.fitconnect.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-//@PropertySource("classpath:application.properties")
 @EnableTransactionManagement
-public class MongoConfig /*extends AbstractMongoClientConfiguration */{
+public class MongoConfig {
 
-//    @Value("${spring.data.mongodb.database}")
-//    private String databaseName;
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
     }
 
-//    @Override
-//    protected String getDatabaseName() {
-//        return databaseName;
-//    }
-
-//    @Override
-//    public MongoClient mongoClient() {
-//        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
-//        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-//                .applyConnectionString(connectionString)
-//                .build();
-//        return MongoClients.create(mongoClientSettings);
-//    }
 }
